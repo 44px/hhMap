76b583cbfbeabbe2fbc1bc7c9941c5b7d2c91f0c
Alexander Shepelin <Brightcor@gmail.com>
2014-06-23 01:36:25 +0400

    API usage samples

--- src/app.js
+++ src/app.js
@@ -1,5 +1,54 @@
 'use strict';
 
-angular.module('hhMap', [
+var app = angular.module('hhMap', [
+	'ngResource',
 	'yaMap'
 ]);
+
+app.constant('API_URL', 'https://api.hh.ru');
+
+app.factory('Vacancies', function(API_URL, $resource) {
+	return $resource(API_URL + '/vacancies/:id', {}, {
+		'query': {
+			method: 'GET',
+			isArray: false
+		}
+	});
+});
+
+
+
+app.controller('MapCtrl', function(Vacancies) {
+
+	// Get new
+	var vacancies = Vacancies.query();
+	vacancies.$promise.then(function(data) {
+		console.group('Latest @ hh.ru:');
+		data.items.forEach(function(elem) {
+			console.log(elem.id, elem.name);
+		});
+		console.groupEnd();
+	});
+
+
+	// Get one
+	var vacancy = Vacancies.get({id:10933376});
+	vacancy.$promise.then(function(data) {
+		console.log('Specific vacancy:');
+		console.log(data.id, data.name);
+	});
+
+
+	// Search
+	var foundVacancies = Vacancies.get({
+		area: 2
+	});
+
+	foundVacancies.$promise.then(function(data) {
+		console.group('Latest in SPB:');
+		data.items.forEach(function(elem) {
+			console.log(elem.id, elem.name);
+		});
+		console.groupEnd();
+	});
+});
